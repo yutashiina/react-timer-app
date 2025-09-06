c39ff2100a460ccdf53d8f6a182099936dddce2d
Yuta Shiina <bluebeat@circus.ocn.ne.jp>
2025-09-06 15:56:14 +0900

    App.tsx：タイトル、ソースへのリンク追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import TimerTabs from './components/TimerTabs';
 const App: React.FC = () => {
   return (
     <div>
+      <h1 style={{textAlign: "center"}}>TIMER</h1>
+      <p style={{textAlign: "center"}}><a href="https://github.com/yutashiina/react-timer-app.git" target="_blank">ソースコード</a></p>
       <TimerTabs />
     </div>
   );
